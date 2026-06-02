4e7aabf1bf16e39039ba743b60f35c653a929d91
juan-gil-05 <gilj06@hotmail.com>
2026-06-02 12:26:14 +0200

    feat: add note management features, including create and delete functionality, and implement loading indicator

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import api from "../api";
 import { useNavigate } from "react-router-dom";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
+import LoadingIndicator from "./LoadingIndicator";
 
 function Form({ route, method }) {
     const [username, setUsername] = useState("")
@@ -37,13 +38,10 @@ function Form({ route, method }) {
             <h1>{name}</h1>
             <input className="form-input" type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
             <input className="form-input" type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
-            <button type="submit">{name}</button>
+            {isLoading && <LoadingIndicator/>}
+            <button type="submit" className="form-button">{name}</button>
         </form>
     )
-
-
-
-
 }
 
 export default Form
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,73 @@
+/* eslint-disable react-hooks/immutability */
+import { useEffect, useState } from "react"
+import api from "../api"
+import Note from "../components/Note"
+import "../styles/Home.css"
+
 function Home() {
-    return <div>Home</div>
+
+    const [notes, setNotes] = useState([])
+    const [content, setContent] = useState("")
+    const [title, setTitle] = useState("")
+
+    useEffect(() => {
+        getNotes()
+    }, [])
+
+    const getNotes = () => {
+        api.get("/api/notes/")
+            .then((res) => res.data)
+            .then((data) => { setNotes(data); console.log(data) })
+            .catch((err) => alert(err))
+    }
+
+    const deleteNote = (id) => {
+        api.delete(`/api/notes/delete/${id}/`)
+            .then((res) => {
+                if (res.status === 204) {
+                    alert("Note deleted!")
+                } else {
+                    alert("Failed to delete the note")
+                }
+                getNotes()
+            })
+            .catch((err) => alert(err))
+    }
+
+    const createNote = (e) => {
+        e.preventDefault()
+        api.post("/api/notes/", { title, content })
+            .then((res) => {
+                if (res.status === 201) {
+                    alert("Note created!")
+                } else {
+                    alert("Failed to create the note")
+                }
+                getNotes()
+            })
+            .catch((err) => alert(err))
+    }
+
+    return <div>
+        <div>
+            <h2>Notes</h2>
+            {notes.map((note) => (
+                <Note note={note} onDelete={deleteNote} key={note.id} />
+            ))}
+        </div>
+        <div>
+            <h3>Create a Note</h3>
+            <form onSubmit={createNote}>
+                <label htmlFor="title">Title</label>
+                <br />
+                <input type="text" name="title" id="title" value={title} onChange={(e) => setTitle(e.target.value)} required />
+                <label htmlFor="content">Content</label>
+                <br />
+                <textarea type="text" name="content" id="content" value={content} onChange={(e) => setContent(e.target.value)} required />
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+    </div>
 }
 
 export default Home
